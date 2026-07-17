3f77d744a44299f560b7beac4c8cc3ec639aa267
anj2609 <anjaligupta25092005@gmail.com>
2026-07-17 17:35:34 +0530

    Add AppColors palette

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFF6C5CE7);
+  static const Color primaryDark = Color(0xFF4834D4);
+  static const Color secondary = Color(0xFF00B894);
+  static const Color accent = Color(0xFFFDCB6E);
+  static const Color danger = Color(0xFFE74C3C);
+  static const Color warning = Color(0xFFF39C12);
+  static const Color success = Color(0xFF00B894);
+  static const Color info = Color(0xFF0984E3);
+
+  static const Color backgroundLight = Color(0xFFF7F8FC);
+  static const Color backgroundDark = Color(0xFF0F1117);
+  static const Color surfaceLight = Color(0xFFFFFFFF);
+  static const Color surfaceDark = Color(0xFF1A1D27);
+
+  static const Color textPrimaryLight = Color(0xFF1A1D27);
+  static const Color textSecondaryLight = Color(0xFF6B7280);
+  static const Color textPrimaryDark = Color(0xFFF7F8FC);
+  static const Color textSecondaryDark = Color(0xFF9CA3AF);
+
+  static const Color border = Color(0xFFE5E7EB);
+  static const Color borderDark = Color(0xFF2D303E);
+
+  static const Color openState = Color(0xFF00B894);
+  static const Color closedState = Color(0xFFE74C3C);
+  static const Color mergedState = Color(0xFF6C5CE7);
+  static const Color draftState = Color(0xFF9CA3AF);
+}
