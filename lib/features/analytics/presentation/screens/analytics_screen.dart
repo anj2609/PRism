03bb9ffa96fecb39ecaa4eb19cbe4d23bbd889e4
anj2609 <anjaligupta25092005@gmail.com>
2026-07-17 17:51:11 +0530

    Remove unused local variable in AnalyticsScreen

--- lib/features/analytics/presentation/screens/analytics_screen.dart
+++ lib/features/analytics/presentation/screens/analytics_screen.dart
@@ -11,7 +11,6 @@ class AnalyticsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(title: const Text('Analytics')),
       body: ListView(
